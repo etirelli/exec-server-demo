f65ee8da22c6f65b5a044847e63d80ed97dc168b
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2016-02-21 21:40:22 -0500

    ops

--- bankapp-showcase/src/main/java/org/kie/demo/bankapp/client/MortgageFormView.java
+++ bankapp-showcase/src/main/java/org/kie/demo/bankapp/client/MortgageFormView.java
@@ -169,14 +169,14 @@ public class MortgageFormView extends Composite
     @Override
     public void setAsApproved() {
         StyleHelper.addUniqueEnumStyleName( approvedForm, ValidationState.class, ValidationState.SUCCESS );
-        setApproved( "No" );
+        setApproved( "Yes" );
         this.approved.getElement().getStyle().setBackgroundColor( "#d5ff80" );
     }
 
     @Override
     public void setAsNotApproved() {
         StyleHelper.addUniqueEnumStyleName( approvedForm, ValidationState.class, ValidationState.ERROR );
-        setApproved( "Yes" );
+        setApproved( "No" );
         this.approved.getElement().getStyle().setBackgroundColor( "#ff6666" );
     }
 
